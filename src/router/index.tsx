1383ce25625bf7525e98a9c44af5454d480714e7
Yurii Dmitriev <yurii.dmitriev@ticketmaster.com>
2020-06-05 18:11:36 +0300

    updated root router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import { Word } from 'pages/Word';
 import { Words } from 'pages/Words';
 
 export const Router: FC = () => (
   <BrowserRouter>
-    <Route exec path="/">
-      <Redirect to="/words" />
-    </Route>
-    <Route path="/words">
-      <Words />
-    </Route>
-    <Route path="/word/:wordName">
-      <Word />
-    </Route>
+    <Switch>
+      <Route path="/words">
+        <Words />
+      </Route>
+      <Route path="/word/:wordName">
+        <Word />
+      </Route>
+      <Route path="/">
+        <Redirect to="/words" />
+      </Route>
+    </Switch>
   </BrowserRouter>
 );
