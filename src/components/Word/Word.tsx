f7233ae301f1e23bd32ddb19756172779576292e
Yurii Dmitriev <yurii.dmitriev@ticketmaster.com>
2020-06-09 09:26:11 +0300

    added WordMemoizedMark

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -1,16 +1,25 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { Popover } from 'antd';
+import { Popover, Icon } from 'antd';
 import { IWord } from 'interfaces/IWord';
 import { WordActions } from './WordActions';
 import * as S from './styles';
+
+const WordMemoizedMark: FC<{ word: string; learned: boolean }> = ({
+  word,
+  learned,
+}) => (
+  <div>
+    {word} {learned && <Icon style={{ color: '#18d218' }} type="check" />}
+  </div>
+);
 
 export const Word: FC<{ words: IWord[]; showInfo: boolean }> = ({
   words,
   showInfo,
 }) => {
   const [firstWord]: IWord[] = words;
-  const { id, word, translate, imageSrc }: IWord = firstWord;
+  const { id, word, translate, imageSrc, learned }: IWord = firstWord;
 
   const wordPageURL = `word/${word}`;
 
@@ -21,7 +30,10 @@ export const Word: FC<{ words: IWord[]; showInfo: boolean }> = ({
       <S.WordCard>
         <S.CardBody>
           <div>
-            <S.Meta title={word} description={showInfo ? translate : null} />
+            <S.Meta
+              title={<WordMemoizedMark word={word} learned={learned} />}
+              description={showInfo ? translate : null}
+            />
             <S.IconWrapper>
               <Popover
                 placement="bottom"
