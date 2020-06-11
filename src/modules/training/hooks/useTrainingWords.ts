481f0ce55eb5a6180109e843248cf9afb644a749
Yurii Dmitriev <yurii.dmitriev@ticketmaster.com>
2020-06-11 16:09:03 +0300

    added trainig animations

---
+++ src/modules/training/hooks/useTrainingWords.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useFetchWordsList } from 'state/fetchWordsList/useFetchWordsList';
+import { IWord } from 'interfaces/IWord';
+
+export const useTrainingWords = () => {
+  const { words, fetchWordsList } = useFetchWordsList();
+  const [trainingWords, setTrainingWords] = useState<IWord[] | null>(null);
+
+  useEffect(() => {
+    fetchWordsList();
+
+    const unlearnedWords: IWord[] = words.filter(word => !word.learned);
+    setTrainingWords(unlearnedWords);
+  }, [words.length]);
+
+  return { trainingWords, setTrainingWords };
+};
--- src/modules/training/page/Training.tsx
+++ src/modules/training/page/Training.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { Button } from 'antd';
-import { useFetchWordsList } from 'state/fetchWordsList/useFetchWordsList';
 import { IWord } from 'interfaces/IWord';
 import { WordsSDK } from 'sdk/WordsSDK';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { useTrainingWords } from '../hooks/useTrainingWords';
 import * as S from './styles';
+import './animation.css';
 
-const useTrainingWords = () => {
-  const { words, fetchWordsList } = useFetchWordsList();
-  const [trainingWords, setTrainingWords] = useState<IWord[] | null>(null);
+const showFirst = (_, index: number): boolean => index === 0;
 
-  useEffect(() => {
-    fetchWordsList();
-
-    const unlearnedWords: IWord[] = words.filter(word => !word.learned);
-    setTrainingWords(unlearnedWords);
-  }, [words.length]);
-
-  return { trainingWords, setTrainingWords };
-};
-
-export const Training = () => {
+export const Training: FC = () => {
   const { trainingWords, setTrainingWords } = useTrainingWords();
 
   const removeTrainingWord = (id: number) => {
@@ -43,33 +33,37 @@ export const Training = () => {
 
   return (
     <>
-      {(trainingWords as IWord[])
-        .filter((i, index) => index === 0)
-        .map(({ id, word, context }) => (
-          <S.Container key={id}>
-            <S.Wrapper>
-              <S.Word>{word}</S.Word>
-              <S.Description>{context}</S.Description>
+      <TransitionGroup>
+        {(trainingWords as IWord[])
+          .filter(showFirst)
+          .map(({ id, word, context }) => (
+            <CSSTransition key={id} timeout={500} classNames="item">
+              <S.Container>
+                <S.Wrapper>
+                  <S.Word>{word}</S.Word>
+                  <S.Description>{context}</S.Description>
 
-              <S.ButtonsWrapper>
-                <Button
-                  onClick={() => handleMemoized(id)}
-                  size="large"
-                  type="primary"
-                >
-                  Know
-                </Button>
-                <Button
-                  onClick={() => removeTrainingWord(id)}
-                  size="large"
-                  type="danger"
-                >
-                  Missed
-                </Button>
-              </S.ButtonsWrapper>
-            </S.Wrapper>
-          </S.Container>
-        ))}
+                  <S.ButtonsWrapper>
+                    <Button
+                      onClick={() => handleMemoized(id)}
+                      size="large"
+                      type="primary"
+                    >
+                      Know
+                    </Button>
+                    <Button
+                      onClick={() => removeTrainingWord(id)}
+                      size="large"
+                      type="danger"
+                    >
+                      Missed
+                    </Button>
+                  </S.ButtonsWrapper>
+                </S.Wrapper>
+              </S.Container>
+            </CSSTransition>
+          ))}
+      </TransitionGroup>
     </>
   );
 };
