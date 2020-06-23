bdd204621fab56586f313f4ee5bd9023a66d838b
Yurii Dmitriev <yurii.dmitriev@ticketmaster.com>
2020-06-23 10:47:28 +0300

    make it better124

--- src/shared/constants/url.ts
+++ src/shared/constants/url.ts
@@ -1,12 +1,12 @@
 export const SERVER_URL: string =
   process.env.NODE_ENV === 'production'
-    ? 'http://english-server.eu-central-1.elasticbeanstalk.com'
+    ? 'https://english-m.herokuapp.com'
     : 'https://english-m.herokuapp.com';
 export const FETCH_WORDS_LIST_URL: string =
   process.env.NODE_ENV === 'production'
-    ? 'http://english-server.eu-central-1.elasticbeanstalk.com/words'
+    ? 'https://english-m.herokuapp.com/words'
     : 'https://english-m.herokuapp.com/words';
 export const FETCH_WORD_URL: string =
   process.env.NODE_ENV === 'production'
-    ? 'http://english-server.eu-central-1.elasticbeanstalk.com/word'
+    ? 'https://english-m.herokuapp.com/word'
     : 'https://english-m.herokuapp.com/word';
