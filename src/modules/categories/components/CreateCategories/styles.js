689c6d60f668add2b07d4b0e1c3548e7dce5216f
Yurii Dmitriev <yurii.dmitriev@ticketmaster.com>
2020-06-24 09:48:28 +0300

    removed categories from mobile

--- src/modules/categories/components/CreateCategories/styles.js
+++ src/modules/categories/components/CreateCategories/styles.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const CreateCategoriesWrapper = styled.div`
   font-size: 34px;
   cursor: pointer;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
 `;
 
 export const InputLayout = styled.div`
