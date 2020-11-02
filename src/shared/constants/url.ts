9350b9bb0ecd18e7d3c6bfe12045604feb56356e
Yurii Dmitriev <yurii.dmitriev@ticketmaster.com>
2020-11-02 12:23:54 +0200

    push build

--- src/shared/constants/url.ts
+++ src/shared/constants/url.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL: string =
   process.env.NODE_ENV === 'production'
     ? 'https://english-m.herokuapp.com'
-    : 'http://localhost:3008';
+    : 'http://3.120.210.76:8081';
